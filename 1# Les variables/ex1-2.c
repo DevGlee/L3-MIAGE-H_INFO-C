cc81ead59d25e7ef6289eb408a370820823e38a0
DevGlee <56635346+DevGlee@users.noreply.github.com>
2020-09-23 11:05:33 -0400

    update

---
+++ 1# Les variables/ex1-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+ int main() {
+
+int e , c, d;
+
+printf("Entrer votre argent en euros");
+
+scanf("%d\n", &e);
+
+printf("Conversion !");
+
+c= e*0.92;
+
+d=c;
+
+printf("Vous avez %d dollars\n", &d);
+
+
+
+
+    return 0;
+}
