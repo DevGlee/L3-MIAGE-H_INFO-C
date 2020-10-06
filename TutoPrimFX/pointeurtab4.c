609bb0b0aca9d6f4dc34485b26de31ec8f51b997
DevGlee <56635346+DevGlee@users.noreply.github.com>
2020-10-05 22:33:45 -0400

    Update pointeurtab4.c

--- TutoPrimFX/pointeurtab4.c
+++ TutoPrimFX/pointeurtab4.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void remplirTableau(int *tab, int size)
+void remplirTableau(int *tab, int size)  //Fonction qui nous permet de remplir un tableau 
 {
     int i;
-    for (i = 0; i < size; i++)
+    for (i = 0; i < size; i++)   // On va parcourir le tableau 
     {
-        *(tab + i) = 100 * i; //tab[i] <=> *(tab i+1)
+        *(tab + i) = 100 * i; //tab[i] <=> *(tab i+1) ainsi on incremente 100 à chaque indice 
     }
 }
 
@@ -17,21 +17,21 @@ int main()
     //tabEntier <=> &(tabEntier[0]) l'équivalent d'un pointeur sur un entier
     //
 
-    tabEntier[0]= 50;
-    tabEntier[1]= 240;
+    tabEntier[0]= 50;  // on met la valeur 50 à l'indice 0 du tableau 
+    tabEntier[1]= 240; //On met la valeur 240 à l'indice 1 du tableau 
 
     printf("La valeur de tabEntier[0] est %d\n",*(tabEntier+0/*Equivaut à tabEntier[0]*/)); 
     printf("La valeur de tabEntier[1] est %d\n",*(tabEntier+1/*Equivaut à tabEntier[1]*/));
     printf("\n ==============\n");
-    for (i = 0; i < taille; i++)
+    for (i = 0; i < taille; i++) // On parcours le tableau 
     {
         printf("tabEntier[%d] = %d\n", i, tabEntier[i]);
     }
 
-    remplirTableau(tabEntier, taille);
+    remplirTableau(tabEntier, taille); // On fait appelle à la fonction  pour remplir notre tableau 
     printf("\n=============\n");
 
-    for (i = 0; i < taille; i++)
+    for (i = 0; i < taille; i++) // On parcours le tableau cette fois ci il sera rempli grâce à la fonction appelé précédemment 
     {
         printf("tabEntier[%d] = %d\n", i, tabEntier[i]);
     }
